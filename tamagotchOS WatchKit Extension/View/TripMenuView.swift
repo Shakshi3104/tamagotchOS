c33cae38b83e9f1d55a08c6a8934e251546a6522
Shakshi3104 <pikanchisato@yahoo.co.jp>
2021-06-20 15:21:18 +0900

    Implement TripMenuView

---
+++ tamagotchOS WatchKit Extension/View/TripMenuView.swift
@@ -0,0 +1,23 @@
+//
+//  TripMenuView.swift
+//  tamagotchOS WatchKit Extension
+//
+//  Created by MacBook Pro on 2021/06/20.
+//
+
+import SwiftUI
+
+struct TripMenuView: View {
+    var body: some View {
+        Form {
+            Text("ひろば")
+            Text("たまワーク")
+        }
+    }
+}
+
+struct TripMenuView_Previews: PreviewProvider {
+    static var previews: some View {
+        TripMenuView()
+    }
+}
